1c85f3ec9e79bbfa7daaa534b4f8ef360f6532f8
qwoprocks <46164638+qwoprocks@users.noreply.github.com>
2019-03-03 22:02:01 +0800

    Changed to large categories, added preprocessing for training data, and changed to bidirectional LSTM

--- data.py
+++ data.py
@@ -5,66 +5,85 @@ import pandas as pd
 
 '''
 When a new instance of TrainingData is created, data is loaded, SHUFFLED and stored in
-x_train, y_train, x_val, and y_val variables
+x_train, y_train variables
 
 The general category, retrieved from the image filepath, is stored in the general_cat variable.
 It is either "beauty", "mobile", or "fashion"
 
+Call class method getTrainingData(category) to get all x_train, y_train, x_val and y_val for that category
+
 ARGS: 
 type - pass in "sentences" to get back a list of sentences for x_train
-	   pass in "words" to get back a list of every single word in the data for x_train
+       pass in "words" to get back a list of every single word in the data for x_train
 
 Example usage:
 td = TrainingData("sentences")
-x_train = td.x_train
-y_train = td.y_train
-
+x_train, y_train, x_val, y_val, output_size = td.getTrainingData("beauty")
 '''
 
 class TrainingData:
-	def __init__(self, validation_percent=0.1, type="sentences"):
-		self.data = pd.read_csv("data/train.csv", delimiter=",")
-		# SHUFFLE
-		self.data = self.data.sample(frac=1).reset_index(drop=True)
-		self.numberOfTrainingSamples = int(round(len(self.data) * (1 - validation_percent)))
+    def __init__(self, validation_percent=0.1, type="sentences"):
+        self.data = pd.read_csv("data/train.csv", delimiter=",")
+        # SHUFFLE
+        self.data = self.data.sample(frac=1).reset_index(drop=True)
+        self.validation_percent = validation_percent
 
-		self.x_train = []
-		self.x_val = []
-		for i, sentence in enumerate(self.data["title"]):
-			if i > self.numberOfTrainingSamples - 1:
-				self.x_val.append(sentence)
-				continue
-			if type == "sentences":
-				self.x_train.append(sentence)
-			elif type == "words":
-				for word in sentence:
-					self.x_train.append(word)
-		
-		self.y_train = []
-		self.y_val = []
-		for i, category in enumerate(self.data["Category"]):
-			if i > self.numberOfTrainingSamples - 1:
-				self.y_val.append(category)
-				continue
-			self.y_train.append(category)
+        self.x_train = []
+        for i, sentence in enumerate(self.data["title"]):
+            if type == "sentences":
+                self.x_train.append(sentence)
+            elif type == "words":
+                for word in sentence:
+                    self.x_train.append(word)
+        
+        self.y_train = []
+        for i, category in enumerate(self.data["Category"]):
+            self.y_train.append(category)
 
-		self.general_cat = []
-		self.general_cat_val = []
-		for i, filepath in enumerate(self.data["image_path"]):
-			if i > self.numberOfTrainingSamples - 1:
-				self.general_cat_val.append(filepath.split("/")[0].split("_")[0])
-				continue
-			self.general_cat.append(filepath.split("/")[0].split("_")[0])
+        self.general_cat = []
+        self.general_cat_val = []
+        for i, filepath in enumerate(self.data["image_path"]):
+            self.general_cat.append(filepath.split("/")[0].split("_")[0])
+
+    def getTrainingData(self, category = 0):
+        indices = []
+        for i, cat in enumerate(self.general_cat):
+            if cat == category:
+                indices.append(i)
+        train = int(round(len(indices) * (1.0 - self.validation_percent)))
+
+        x_train = []
+        y_train = []
+        x_val = []
+        y_val = []
+        
+        for i, index in enumerate(indices):
+            if i > train - 1:
+                x_val.append(self.x_train[index])
+                y_val.append(self.y_train[index])
+                continue
+            x_train.append(self.x_train[index])
+            y_train.append(self.y_train[index])
+
+        # CONVERT Y TO SET OF INDICES WITHIN THE CATEGORY ONLY
+        minimum = min(s for s in y_train)
+        y_train = [(y - minimum) for y in y_train]
+        y_val = [(y - minimum) for y in y_val]
+
+        output_size = len(set(y_val))
+
+        return x_train, y_train, x_val, y_val, output_size
+
 
 
 class TestData:
-	def __init__(self):
-		self.data = pd.read_csv("data/test.csv", delimiter=",")
-		
-		self.x_test = []
-		for sentence in self.data["title"]:
-			self.x_test.append(sentence)
+    def __init__(self):
+        self.data = pd.read_csv("data/test.csv", delimiter=",")
+        
+        self.x_test = []
+        for sentence in self.data["title"]:
+            self.x_test.append(sentence)
 
-		self.general_cat = []
-		for filepath in self.data["image_path"]:
-			self.general_cat.append(filepath.split("/")[0].split("_")[0])
+        self.general_cat = []
+        for filepath in self.data["image_path"]:
+            self.general_cat.append(filepath.split("/")[0].split("_")[0])
--- text_pytorch.py
+++ text_pytorch.py
@@ -2,6 +2,7 @@
 
 import pickle
 import random
+import Levenshtein
 
 import torch
 import torch.nn as nn
@@ -15,15 +16,17 @@ from data import TrainingData
 # Use CUDA if available -> replacing every .cuda() with .to(device)
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
+
+# ADJUSTABLE PARAMETERS
 validation_percent = 0.1
+category = "fashion"
+chunk_size = 20000
+
 
 # LOAD TRAINING DATA
+print("Loading training data")
 td = TrainingData(validation_percent)
-x_train = td.x_train
-y_train = td.y_train
-x_val = td.x_val
-y_val = td.y_val
-
+x_train, y_train, x_val, y_val, output_size = td.getTrainingData(category)
 
 # LOAD WORD2VEC DATA
 print("Loading dictionary of embeddings")
@@ -34,7 +37,52 @@ f.close()
 words = list(dictionary.keys())
 
 
+# PROCESS TRAINING DATA
+print("Processing training data")
+x_train_chunked = [x_train[i:i + chunk_size] for i in range(0, len(x_train), chunk_size)]
+
+def processSentence(sentence):
+    newX = []
+    for word in sentence.split(" "):
+        if word in words:
+            newX.append(words.index(word))
+        else:
+            jaros = [Levenshtein.jaro(word, w) for w in words]
+            highest_index = jaros.index(max(jaros))
+            newX.append(highest_index)
+    newX = torch.tensor(newX).to(device).long()
+    return newX
+
+current_training_example = -1
+current_chunk = 0
+def processChunk(current_chunk):
+    global x_train
+    x_train = [processSentence(sentence) for sentence in x_train_chunked[current_chunk]]
+processChunk(current_chunk)
+
+def getNextTrainingExample():
+    global current_training_example
+    global current_chunk
+    current_training_example += 1
+    if current_training_example > len(x_train) - 1:
+        current_chunk += 1
+        processChunk(current_chunk % len(x_train_chunked))
+        current_training_example = -1
+
+    newY = torch.tensor([y_train[current_training_example + current_chunk * chunk_size]]).to(device)
+    return x_train[current_training_example], newY.long()
+
+# PROCESS VALIDATION DATA
+print("Processing validation data")
+processed_x_val = []
+processed_y_val = []
+for i, sentence in enumerate(x_val):
+    processed_x_val.append(processSentence(sentence))
+    processed_y_val.append(torch.tensor([y_val[i]]).to(device).long())
+
+
 # COPY WORD2VEC EMBEDDINGS INTO NN.EMBEDDING LAYER
+print("Copying embeddings")
 vocab_size = len(dictionary)
 vector_size = 100
 pretrained_weights = list(dictionary.values())
@@ -55,10 +103,10 @@ class LSTM(nn.Module):
         super(LSTM, self).__init__()
 
         self.hidden_size = hidden_size
-        self.lstm = nn.LSTM(input_size, hidden_size).to(device)
+        self.lstm = nn.LSTM(input_size, hidden_size, bidirectional=True).to(device)
 
         self.embeddings = embeddings
-        self.fc = nn.Linear(hidden_size, output_size).to(device)
+        self.fc = nn.Linear(hidden_size * 2, output_size).to(device)
         self.hidden = self.initHidden()
 
     def forward(self, input):
@@ -69,13 +117,17 @@ class LSTM(nn.Module):
         return out
 
     def initHidden(self):
-        return (torch.zeros(1, 1, self.hidden_size).to(device), torch.zeros(1, 1, self.hidden_size).to(device))
+        return (torch.zeros(2, 1, self.hidden_size).to(device), torch.zeros(2, 1, self.hidden_size).to(device))
 
 
-lstm = LSTM(embed, 100, 200, 58)
+
+# MODEL PARAMETERS
+hidden_size = 100
+lr = 0.01
+
+lstm = LSTM(embed, 100, hidden_size, output_size).to(device)
 
 criterion = nn.CrossEntropyLoss().to(device)
-lr = 0.01
 optimizer = optim.Adam(lstm.parameters(), lr)
 
 def train(x, y):
@@ -98,10 +150,10 @@ def train(x, y):
 import time
 import math
 
-n_iters = len(x_train) * 10
+n_iters = (len(x_train_chunked) * (chunk_size - 1) + len(x_train_chunked[-1])) * 10
 print_every = 1000
 validate_every = 5000
-decay_every = 15000
+decay_every = len(x_train)
 plot_every = 50
 
 # Keep track of losses for plotting
@@ -117,50 +169,31 @@ def timeSince(since):
 
 start = time.time()
 
-# THIS FUNCTION WILL RETURN ONE RANDOM SET OF (INPUT, EXPECTED OUTPUT)
-# TAKEN FROM THE TRAINING DATA. CURRENTLY DOES ON-THE-FLY PROCESSING OF
-# THE INPUT, MIGHT WANT TO MOVE THE PROCESSING TO BEFORE TRAINING
-def randomTrainingExample():
-	i = random.randint(0, len(x_train) - 1)
-	newX = []
-	for word in x_train[i].split(" "):
-		if word in words:
-			newX.append(words.index(word))
 
-	newY = torch.tensor([y_train[i]]).to(device)
-	return torch.tensor(newX).to(device).long(), newY.long()
-
-
-print("Training the model")
-print("Number of iterations:", n_iters)
+print("Training the model\n")
+print("CATEGORY:", category)
+print("NO OF CLASSES:", output_size)
 
 for iter in range(1, n_iters + 1):
 	# Get a random example and train
-    x, y = randomTrainingExample()
+    x, y = getNextTrainingExample()
     if len(x) == 0:
         continue
     output, loss = train(x, y)
     current_loss += loss
 
-    # Print iter number, loss, name and guess
+    # Print iter number, loss
     if iter % print_every == 0:
-        print("Iter: %6d %5.2f%% %s" % (iter, iter / n_iters * 100.0, timeSince(start)), "trg_loss:", loss)
+        print("Iter: %6d / %7d %5.2f%% %s" % (iter, n_iters, iter / n_iters * 100.0, timeSince(start)), "trg_loss:", loss)
 
     # Validate
     if iter % validate_every == 0:
         correct = 0
         losses = 0
-        for i, x in enumerate(x_val):
-            newX = []
-            for word in x.split(" "):
-                if word in words:
-                    newX.append(words.index(word))
-            newX = torch.tensor(newX).to(device).long()
-            if len(newX) == 0:
-            	continue
-            output = lstm(newX)
-            losses += criterion(output, torch.tensor([y_val[i]]).to(device).long()).item()
-            if torch.max(output[0], 0)[1].item() == y_val[i]:
+        for i, x in enumerate(processed_x_val):
+            output = lstm(x)
+            losses += criterion(output, processed_y_val[i]).item()
+            if torch.max(output[0], 0)[1].item() == processed_y_val[i].item():
                 correct += 1
         correct = correct / len(x_val) * 100
         loss = losses / len(x_val)

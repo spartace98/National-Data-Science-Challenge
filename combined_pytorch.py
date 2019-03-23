907b40633ff4f06ff6d4032f8da8d4c3eb813065
qwoprocks <46164638+qwoprocks@users.noreply.github.com>
2019-03-23 15:56:01 +0800

    Update combined_pytorch.py

--- combined_pytorch.py
+++ combined_pytorch.py
@@ -105,11 +105,19 @@ for cat in ["fashion", "beauty", "mobile"]:
     else: 
         model = LSTM(embed, 100, 100, output_size)
     model.load_state_dict(torch.load("models/"+cat+".text.pth"))
+    model.to(device)
+    for param in model.parameters():
+        param.requires_grad = False
+    
     model.eval()
     print(model)
 
+    print("Processing training text")
+
     x_train = [model(processSentence(sentence)) for sentence in x_train]
     y_train = [torch.tensor(y).to(device).long() for y in y_train]
+
+    print("Processing validation text")
 
     x_val = [model(processSentence(sentence)) for sentence in x_val]
     y_val = [torch.tensor(y).to(device).long() for y in y_val]
@@ -117,14 +125,14 @@ for cat in ["fashion", "beauty", "mobile"]:
     # ADD UR IMAGE CODE HERE, PROCESS IMAGE TRAIN AND IMAGE VAL AND OUTPUT A [0, 0, 0.... 1, 0, 0] TENSOR
     # @LICHAO
 
-    image_train_X, image_train_y, image_val_X, image_val_y, output_size = td.getTrainingImages(cat)
+    image_train_X, _, image_val_X, _, output_size = td.getTrainingImages(cat)
 
-    dset_train = DatasetProcessing(image_train_X, image_train_y, image_transform)
+    dset_train = DatasetProcessing(image_train_X, [], image_transform)
     train_loader = torch.utils.data.DataLoader(dset_train, batch_size=batch_size,
                                           shuffle=True, num_workers=0)
 
 
-    dset_val = DatasetProcessing(image_val_X, image_val_y, image_transform)
+    dset_val = DatasetProcessing(image_val_X, [], image_transform)
     val_loader = torch.utils.data.DataLoader(dset_val, batch_size=batch_size,
                                           shuffle=True, num_workers=0)
 
@@ -146,34 +154,26 @@ for cat in ["fashion", "beauty", "mobile"]:
 
     nb_batches = round(len(image_train_X) / batch_size)
     image_X_train = []
-    image_y_train = []
     image_X_val = []
-    image_y_val = []
 
     for i, (x_temp, y_temp) in enumerate(train_loader):
         output = image_model(x_temp)
         image_X_train.append(output)
-        image_y_train.append(y_temp)
         print('Progress:', 100 * i / nb_batches, '%')
 
         if len(x_temp) < batch_size:
             break
 
     image_X_train = torch.cat((image_X_train), 0)
-    image_y_train = torch.cat((image_y_train), 0)
-    print(image_X_train.size(), image_y_train.size())
 
     for i, (x_temp, y_temp) in enumerate(val_loader):
         output = image_model(x_temp)
         image_X_val.append(output)
-        image_y_val.append(y_temp)
         print('Progress:', 100 * i / nb_batches, '%')
 
         if len(x_temp) < batch_size:
             break
 
     image_X_val = torch.cat((image_X_val), 0)
-    image_y_val = torch.cat((image_y_val), 0)
-    print(image_X_val.size(), image_y_val.size())
 
-    pickle.dump([x_train, y_train, x_val, y_val], open("data/" + cat + ".pickle", "wb"))
+    pickle.dump([x_train, image_X_train, y_train, x_val, image_X_val, y_val], open("data/" + cat + ".pickle", "wb"))

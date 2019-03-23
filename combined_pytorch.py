3d6e6509f019ea95bf98def2f4bfa8bcb52d54cb
qwoprocks <46164638+qwoprocks@users.noreply.github.com>
2019-03-23 12:57:49 +0800

    Update combined_pytorch.py

--- combined_pytorch.py
+++ combined_pytorch.py
@@ -18,6 +18,30 @@ device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 validation_percent = 0.1
 
+# LOAD WORD2VEC DATA
+print("Loading dictionary of embeddings")
+f = open("data/dict.pkl", "rb")
+dictionary = pickle.load(f)
+f.close()
+
+words = list(dictionary.keys())
+
+# COPY WORD2VEC EMBEDDINGS INTO NN.EMBEDDING LAYER
+print("Copying embeddings")
+vocab_size = len(dictionary)
+vector_size = 100
+pretrained_weights = list(dictionary.values())
+# vocab_size is the number of words in your train, val and test set
+# vector_size is the dimension of the word vectors you are using
+embed = nn.Embedding(vocab_size, vector_size).to(device)
+
+# intialize the word vectors, pretrained_weights is a
+# numpy array of size (vocab_size, vector_size) and
+# pretrained_weights[i] retrieves the word vector of
+# i-th word in the vocabulary
+embed.weight.data.copy_(torch.tensor(pretrained_weights).to(device))
+
+
 def processSentence(sentence):
     newX = []
     for word in sentence.split(" "):
@@ -30,31 +54,54 @@ def processSentence(sentence):
     newX = torch.tensor(newX).to(device).long()
     return newX
 
-try:
-    processed = pickle.load(open("data/processed.pickle", "rb"))
-except (OSError, IOError) as e:
-    td = TrainingData(validation_percent)
-    x_combined = []
-    y_combined = []
+# DECLARE LSTM CLASS
+class LSTM(nn.Module):
+    def __init__(self, embeddings, input_size, hidden_size, output_size):
+        super(LSTM, self).__init__()
 
-    for cat in ["beauty", "fashion", "mobile"]:
-        x_train, y_train, x_val, y_val, output_size = td.getTrainingData(cat)
+        self.hidden_size = hidden_size
+        self.lstm = nn.LSTM(input_size, hidden_size, bidirectional=True).to(device)
 
-        # LOAD WORD2VEC DATA
-        print("Loading dictionary of embeddings")
-        f = open("data/dict.pkl", "rb")
-        dictionary = pickle.load(f)
-        f.close()
+        self.embeddings = embeddings
+        self.fc = nn.Linear(hidden_size * 2, output_size).to(device)
+        self.hidden = self.initHidden()
 
-        words = list(dictionary.keys())
+    def forward(self, input):
+        embed = self.embeddings(input)
+        embed = embed.unsqueeze(1)
+        lstm_out, _ = self.lstm(embed, self.hidden)
+        out = F.log_softmax(self.fc(lstm_out[-1]), dim=1).to(device)
+        return out
 
-        x_train = [processSentence(sentence) for sentence in x_train]
-        y_train = [torch.tensor(y).to(device).long() for y in y_train]
+    def initHidden(self):
+        return (torch.zeros(2, 1, self.hidden_size).to(device), torch.zeros(2, 1, self.hidden_size).to(device))
 
-        x_val = [processSentence(sentence) for sentence in x_val]
-        y_val = [torch.tensor(y).to(device).long() for y in y_val]
+td = TrainingData(validation_percent)
+
+for cat in ["fashion", "beauty", "mobile"]:
+    x_train, y_train, x_val, y_val, output_size = td.getTrainingData(cat)
+
+    print(cat)
+
+    if cat == "fashion":
+        model = LSTM(embed, 100, 150, output_size)
+    else: 
+        model = LSTM(embed, 100, 100, output_size)
+    model.load_state_dict(torch.load("models/"+cat+".text.pth"))
+    model.eval()
+    print(model)
+
+    x_train = [model(processSentence(sentence)) for sentence in x_train]
+    y_train = [torch.tensor(y).to(device).long() for y in y_train]
+
+    x_val = [model(processSentence(sentence)) for sentence in x_val]
+    y_val = [torch.tensor(y).to(device).long() for y in y_val]
 
 
-        image_train, _, image_val, _ = td.getTrainingImages(cat)
+    image_train, _, image_val, _ = td.getTrainingImages(cat)
 
-    pickle.dump([x_combined, y_combined, x_val, y_val], open("data/processed.pickle", "wb"))
+    # ADD UR IMAGE CODE HERE, PROCESS IMAGE TRAIN AND IMAGE VAL AND OUTPUT A [0, 0, 0.... 1, 0, 0] TENSOR
+    # @LICHAO
+
+
+    pickle.dump([x_train, y_train, x_val, y_val], open("data/" + cat + ".pickle", "wb"))
